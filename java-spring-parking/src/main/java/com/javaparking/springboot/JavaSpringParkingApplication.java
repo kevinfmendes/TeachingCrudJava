fc06e73acba30146b37049d6347d7be16cb4edac
Kevin Mendes <keviin.nicolas18@gmail.com>
2023-11-03 21:45:21 -0300

    BUILD: add project config mysql

---
+++ java-spring-parking/src/main/java/com/javaparking/springboot/JavaSpringParkingApplication.java
@@ -0,0 +1,13 @@
+package com.javaparking.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringParkingApplication.class, args);
+	}
+
+}
---
+++ java-spring-parking/src/test/java/com/javaparking/springboot/JavaSpringParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaparking.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
